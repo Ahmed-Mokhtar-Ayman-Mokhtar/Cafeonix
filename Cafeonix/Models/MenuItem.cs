59607648d42e291f30b94f50e721f51330fa299b
Ayman-Mokhtar535 <ayman734535800@gmail.com>
2025-05-24 07:22:40 +0300

    Impeleminting MenuPage

---
+++ Cafeonix/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeonix.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }  // مسار الصورة داخل Resources
+        public string Category { get; set; }
+    }
+}
